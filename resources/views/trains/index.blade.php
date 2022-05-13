95027a255741cef642e362a0c0bcb1107d697219
gatsuBerserk <berserkdev@outlook.it>
2022-05-13 15:56:13 +0200

    start-scaffolding

---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,13 @@
+{{-- lista dei treni --}}
+
+@extends('layouts.main')
+
+@section('title', "Laravel seed")
+
+@section('AllTrains')
+    <div>
+        <pre> 
+            All trains 
+        </pre>
+    </div>
+@endsection
