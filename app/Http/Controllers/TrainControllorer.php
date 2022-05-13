d7ab2064bb7099ac5c8aa3ff0cc272a93cd4be35
gatsuBerserk <berserkdev@outlook.it>
2022-05-13 19:40:08 +0200

    controller function

--- app/Http/Controllers/TrainControllorer.php
+++ app/Http/Controllers/TrainControllorer.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Train;
+
 use Illuminate\Http\Request;
 
 class TrainControllorer extends Controller
 {
     public function index(){
-        return view ("trains.index");
+        
+        // $trains = Train::all();
+    //     $data =[
+    //         "train" => new Train(), 
+    //         "trains"=> $trains
+    // ];
+    $trains = Train::where("departure_date", date("y-m-d"))
+    ->get();
+        return view ("trains.index", compact("trains"));
     }
 }
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -6,8 +6,46 @@
 
 @section('AllTrains')
     <div>
-        <pre> 
-            All trains 
-        </pre>
+       <h1>
+           Treni in partenza oggi:
+       </h1>
     </div>
+       @foreach ($trains as $train)
+       <div class="ticket">
+           <div class="company">
+               <h4>
+                   {{$train->company}}
+               </h4>
+           </div>
+           <div class="travel">
+                <p>
+                    <pre>Partenza da:</pre>
+                    {{$train->departure_station}}
+                </p> 
+                <p>
+                    <pre>Orario Partenza</pre>
+                    {{$train->departure_time}}
+                </p>
+                <p>
+                    <pre>Orario Arrivo </pre>
+                    {{$train->arrival_time}}
+                </p>
+                <p>
+                    <pre> Per:</pre>
+                    {{$train->arrival_station}}
+                </p>
+           </div>
+           <div class="train-code">
+                <p>
+                    <pre> Codice treno</pre>
+                    {{$train->train_code}}
+                </p>
+            </div>
+            <div class="info">
+                
+            </div>
+       </div>
+           
+       @endforeach
+    
 @endsection
