b8a5186fd85e7ef70e21eae2c0df81e4541cf175
gatsuBerserk <berserkdev@outlook.it>
2022-05-16 18:43:13 +0200

    style index trains page

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -5,47 +5,36 @@
 @section('title', "Laravel seed")
 
 @section('AllTrains')
-    <div>
-       <h1>
-           Treni in partenza oggi:
-       </h1>
-    </div>
-       @foreach ($trains as $train)
-       <div class="ticket">
-           <div class="company">
-               <h4>
-                   {{$train->company}}
-               </h4>
-           </div>
-           <div class="travel">
-                <p>
-                    <pre>Partenza da:</pre>
-                    {{$train->departure_station}}
-                </p> 
-                <p>
-                    <pre>Orario Partenza</pre>
-                    {{$train->departure_time}}
-                </p>
-                <p>
-                    <pre>Orario Arrivo </pre>
-                    {{$train->arrival_time}}
-                </p>
-                <p>
-                    <pre> Per:</pre>
-                    {{$train->arrival_station}}
-                </p>
-           </div>
-           <div class="train-code">
-                <p>
-                    <pre> Codice treno</pre>
-                    {{$train->train_code}}
-                </p>
-            </div>
-            <div class="info">
+      
+        <div class="container fluid">
+            <div class="row">
+                @foreach ($trains as $train)
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-header d-flex align-items-center">
+                            <h4>
+                                Compagnia: {{$train->company}}
+                            </h4>
+                        </div>
+                        <div class="card-body"> 
+                            <h5>
+                                Da: {{$train->departure_station}}
+                            </h5>
+                            <h5>
+                                Per: {{$train->arrival_station}}
+                            </h5>
+                            <p>Data Partenza: {{$train->departure_date}} </p>
+                            <p>
+                                Ora Partenza: {{$train->departure_time}}
+                            </p> 
+                            <h6>
+                                Codice Treno: {{$train->train_code}}
+                            </h6>
+                        </div>
+                      </div>
+                </div>
                 
+                @endforeach
             </div>
-       </div>
-           
-       @endforeach
-    
+        </div>
 @endsection
