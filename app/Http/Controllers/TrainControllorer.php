400e6acb84d2daee084372ea1b62ba7603c460ad
gatsuBerserk <berserkdev@outlook.it>
2022-05-16 17:19:38 +0200

    trains created and inserted in the DB

--- app/Http/Controllers/TrainControllorer.php
+++ app/Http/Controllers/TrainControllorer.php
@@ -9,14 +9,7 @@ use Illuminate\Http\Request;
 class TrainControllorer extends Controller
 {
     public function index(){
-        
-        // $trains = Train::all();
-    //     $data =[
-    //         "train" => new Train(), 
-    //         "trains"=> $trains
-    // ];
-    $trains = Train::where("departure_date", date("y-m-d"))
-    ->get();
+        $trains = Train::all();    
         return view ("trains.index", compact("trains"));
     }
 }
--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
+use App\Train;
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -9,8 +10,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i ++){
+            $train = new Train();  
+            $train->company = $faker->word(1, true);
+             $train->departure_date =$faker->dateTimeBetween('now', '+1 days'); ;
+             $train->departure_station = $faker-> city();
+             $train->arrival_station =  $faker-> city();
+             $train->departure_time = $faker-> time();
+             $train->arrival_time = $faker-> time(); 
+             $train->train_code = $faker-> regexify('[A-Z]{5}'); 
+             $train->carriages = $faker->randomNumber(2, true);
+             $train->is_onTime = $faker-> boolean(); ; 
+             $train->is_deleted = $faker->boolean();
+             $train->save();
+        }
     }
 }
